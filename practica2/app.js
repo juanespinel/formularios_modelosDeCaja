766f8c1ba6d1e4671fd1e3d2d29948e2763d0e21
Juan <juanespinel@gmail.com>
2020-05-21 22:40:36 -0300

    start

---
+++ practica2/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('craftsy.ejs');
+})
+
+app.listen(3000, function(){
+    console.log('El servidor corriendo en el puerto 3000');
+    
+})
