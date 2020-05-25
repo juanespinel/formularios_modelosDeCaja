5663ff7822380044e45c8378defcaeb21902854a
juanespinel <juanespinel@gmail.com>
2020-05-25 17:25:49 -0300

    Avance III

--- practica2/app.js
+++ practica2/app.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
     res.render('craftsy.ejs');
 })
 
+app.get('/login', function (req, res) {
+    res.render('login.ejs');
+})
+
+app.get('/registro', function (req, res) {
+    res.render('registro.ejs');
+})
+
 app.listen(3000, function(){
     console.log('El servidor corriendo en el puerto 3000');
     
